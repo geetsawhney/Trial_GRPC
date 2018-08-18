4eb49e31f81ce173d28fa77bf56994cde220e590
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-08-17 22:18:39 -0700

    Beautifying the code

--- dict_add_client.py
+++ dict_add_client.py
@@ -10,11 +10,11 @@ from proto import dict_add_pb2
 from proto import dict_add_pb2_grpc
 from google.protobuf import struct_pb2
 
-avaluo = {'origin': 'Google','banos':None, 'floors': 1}
+avaluo = {'origin': 'Google', 'banos': None, 'floors': 1}
 
-st=struct_pb2.Struct()
+st = struct_pb2.Struct()
 for key in avaluo.keys():
-    st[key]=avaluo[key]
+    st[key] = avaluo[key]
 
 channel = grpc.insecure_channel('localhost:50003')
 stub = dict_add_pb2_grpc.DictAddStub(channel)
--- dict_add_server.py
+++ dict_add_server.py
@@ -4,22 +4,24 @@ import time
 
 import grpc
 
-from proto import dict_add_pb2 , dict_add_pb2_grpc
+from proto import dict_add_pb2, dict_add_pb2_grpc
 from google.protobuf import struct_pb2
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
 
 class DictAddServicer(dict_add_pb2_grpc.DictAddServicer):
 
     def Add_Dict(self, request, context):
         print(context)
-        server_dict=dict(request.s)
+        server_dict = dict(request.s)
         print(server_dict)
-        server_dict['owner']= 1.11001
-        st=struct_pb2.Struct()
+        server_dict['owner'] = 1.11001
+        st = struct_pb2.Struct()
         for key in server_dict.keys():
-            st[key]=server_dict[key]
+            st[key] = server_dict[key]
         return dict_add_pb2.Dict(s=st)
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
